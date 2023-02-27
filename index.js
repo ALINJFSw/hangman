45667c2cbcfc8be97de883db5ebaa1cbcacaedd8
Ali Janbein <alijanbein999@gmail.com>
2023-02-27 22:35:19 +0200

    add dictionary after forgetting to use it so i repeat evrithing

--- index.js
+++ index.js
@@ -1,41 +1,58 @@
-const WORDS = [
-  "executioner",
-  "headsman",
-  "murderer",
-  "assassin",
-  "executor",
-  "butcher",
-  "deathsman",
-  "decapitator",
-];
-
-const replace = (word,replacement, index) => {
-  return word.substring(0, index) + replacement + word.substring(index + 1)
+const getRandomWord = () => {
+  const WORDS = [
+    "executioner",
+    "headsman",
+    "murderer",
+    "assassin",
+    "executor",
+    "butcher",
+    "deathsman",
+    "decapitator",
+  ];
+  const r = Math.floor(Math.random() * WORDS.length);
+  return WORDS[r];
 };
 
+
+// const replace = (word, replacement, index) => {
+//   return word.substring(0, index) + replacement + word.substring(index + 2);
+// };
+
+const setWordBoard = (word) => {
+    let wordBoard = ""
+    for(const letter in word){
+      if (!word[letter] ){
+        wordBoard += "_ "
+      }
+      else{
+        wordBoard += letter
+      }
+    }
+    const board = document.getElementById("board");
+    board.innerHTML = wordBoard
+}
+
 const start = () => {
-  const r = Math.floor(Math.random() * WORDS.length);
-  const choisen = WORDS[r];
   const image = document.getElementById("image");
   image.src = "./assets/1.png";
-  const board = document.getElementById("board");
-  let s = "";
-  console.log(choisen);
-  for (let i = 0; i < choisen.length; i++) {
-    s += "_ ";
-  }
-  board.innerHTML = s;
+  
+  
   document.addEventListener("keypress", (event) => {
-    const key = event.key;
-    for (let i = 0; i < choisen.length; i++) {
-      if (key == choisen[i])
-      {
-         s = replace(s,choisen[i],i)
-        board.innerHTML = s
+    const choisen = getRandomWord();
+    const answer = {};
 
+    for (let i = 0; i < choisen.length; i++) {
+      answer[choisen[i]] = false;
+    }
+
+    console.log(answer);
+    const key = event.key;
+    for (const letter in answer) {
+      if (key == letter) {
+        answer.letter = true;
+        setWordBoard();
       }
     }
   });
-
 };
 start();
